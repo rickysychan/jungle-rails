c54632f4e6d79e89bd23a41551b45403b715f933
Ricky Chan <rickysychan@gmail.com>
2017-10-17 17:00:21 +0000

    product features test complete

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to product page", type: :feature, js: true do
+  
+    # SETUP
+    before :each do
+      @category = Category.create! name: 'Apparel'
+  
+      10.times do |n|
+        @category.products.create!(
+          name:  Faker::Hipster.sentence(3),
+          description: Faker::Hipster.paragraph(4),
+          image: open_asset('apparel1.jpg'),
+          quantity: 10,
+          price: 64.99
+        )
+      end
+    end
+  
+    scenario "They see only the selected product" do
+      # ACT
+      visit root_path
+      first(".item").click
+  
+      # DEBUG
+      save_screenshot
+  
+      # VERIFY
+      expect(page).to have_css 'section.products-show'
+    end
+  end
